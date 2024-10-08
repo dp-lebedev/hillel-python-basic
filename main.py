2e8644a7074003efaf31815cee287435077e0465
Дмитро Лебедєв <lotustest@ua.fm>
2024-10-08 21:09:07 +0300

    First PY project version.

---
+++ main.py
@@ -0,0 +1,18 @@
+# print("Hello, world!")
+# snum = 5+5
+# print(5+5)
+
+'''
+test comment
+'''
+
+
+
+# print("Hello\\n\\t\"tworld!\"")
+# print()
+# print('test')
+# print('\\\\\\\\')
+
+print("Hello world", "Text1", "Text2", sep=", ", end=" - ")
+print("Hello world!")
+print("test")
