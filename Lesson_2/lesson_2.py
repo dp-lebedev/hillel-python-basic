f9a2f65e574919db448b61df178ba62136c2821b
Дмитро Лебедєв <lotustest@ua.fm>
2024-10-11 21:00:01 +0300

    Second lesson.

--- Lesson_2/lesson_2.py
+++ Lesson_2/lesson_2.py
@@ -1,17 +1,22 @@
 # 274 => 2 7 4
 
-number = 667
-n1 = number // 100
+# number = 667
+# n1 = number // 100
+#
+# n2 = number % 100 // 10
+#
+# n3 = number % 10
+#
+# sum_n = n1 + n2 + n3
+#
+# print (number)
+# print (n1)
+# print (n2)
+# print (n3)
+# print (sum_n)
 
-n2 = number % 100 // 10
-
-n3 = number % 10
-
-sum_n = n1 + n2 + n3
-
-print (number)
-print (n1)
-print (n2)
-print (n3)
-print (sum_n)
-
+####
+first_number = int(input ("Enter first number: "))
+second_number = int(input ("Enter second number: "))
+result = first_number + second_number
+print ("Result: ", result)
