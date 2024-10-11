2211bd3c945f688411efc5924a6e888a9fdb7330
Дмитро Лебедєв <lotustest@ua.fm>
2024-10-11 21:02:21 +0300

    Second lesson.

--- Lesson_2/lesson_2.py
+++ Lesson_2/lesson_2.py
@@ -15,7 +15,7 @@
 # print (n3)
 # print (sum_n)
 
-####
+###
 first_number = int(input ("Enter first number: "))
 second_number = int(input ("Enter second number: "))
 result = first_number + second_number
