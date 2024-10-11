1c0e1b147e1c5938aeab001f58b7260a37edad7b
Дмитро Лебедєв <lotustest@ua.fm>
2024-10-11 22:22:49 +0300

    Second lesson.

--- Lesson_2/Homework_2.py
+++ Lesson_2/Homework_2.py
@@ -1,7 +1,7 @@
 #### ДЗ 2.1
 input_4number = input ("Введіть чотиризначне число: ")
-# Перетворення на int окремою змінною необхідно для коректного відображення числа, якщо перші декілька введених
-# символів - то нули (наприклад, 0057).
+# Перетворення на int окремою змінною необхідно для коректного відображення введеного числа, якщо перші декілька
+# введених символів - то нули (наприклад, 0057).
 input_4number_int = int(input_4number)
 input_digit_4 = input_4number_int % 10
 input_digit_3 = (input_4number_int % 100 - input_digit_4) / 10
@@ -22,3 +22,18 @@ print("Цифра в розряді одиниць:", int(input_digit_4))
 
 
 #### ДЗ 2.2
+input_5number = input("Введіть п'ятизначне число: ")
+# Перетворення на int окремою змінною необхідно для коректного відображення введеного числа, якщо перші декілька
+# введених символів - то нули (наприклад, 00578).
+input_5number_int = int(input_5number)
+input_digit_1 = input_5number_int // 10000
+input_digit_2 = input_5number_int // 1000 - input_digit_1 * 10
+input_digit_3 = input_5number_int // 100 - input_digit_2 * 10 - input_digit_1 * 100
+input_digit_4 = input_5number_int // 10 - input_digit_3 * 10 - input_digit_2 * 100 - input_digit_1 * 1000
+input_digit_5 = input_5number_int % 10
+# Збираємо число з введених цифр у дзеркальному відображенні
+input_5number_mirrored = input_digit_5 * 10000 + input_digit_4 * 1000 + input_digit_3 * 100 + input_digit_2 * 10 + input_digit_1
+
+print("Ви ввели число:", input_5number)
+print()
+print("Ваше число у дзеркальному відображенні:", input_5number_mirrored)
