ba782e1754daa04e078510e4a988abe8dd4daea6
Дмитро Лебедєв <lotustest@ua.fm>
2024-10-11 21:57:21 +0300

    Second lesson.

--- Lesson_2/Homework_2.py
+++ Lesson_2/Homework_2.py
@@ -1,13 +1,16 @@
 #### ДЗ 2.1
-input_number = int(input ("Enter four-digit number: "))
-input_digit_4 = input_number % 10
-input_digit_3 = (input_number % 100 - input_digit_4) / 10
-input_digit_2 = (input_number % 1000 - input_digit_3*10 - input_digit_4) /100
-input_digit_1 = input_number // 1000
+input_4number = input ("Введіть чотиризначне число: ")
+# Перетворення на int окремою змінною необхідно для коректного відображення числа, якщо перші декілька введених
+# символів - то нули (наприклад, 0057).
+input_4number_int = int(input_4number)
+input_digit_4 = input_4number_int % 10
+input_digit_3 = (input_4number_int % 100 - input_digit_4) / 10
+input_digit_2 = (input_4number_int % 1000 - input_digit_3*10 - input_digit_4) /100
+input_digit_1 = input_4number_int // 1000
 
 # Перетворення на int зроблено, оскільки для input_digit_2 та input_digit_3 тип поля з результатом обчислень був float.
 # Для input_digit_1 та input_digit_4 це було зроблено для того, щоб код був уніфікованим для всієї програми.
-print("Ви ввели число:", input_number)
+print("Ви ввели число:", input_4number)
 print()
 print("Цифра в розряді тисяч:", int(input_digit_1))
 print()
@@ -16,3 +19,6 @@ print()
 print("Цифра в розряді десятків:", int(input_digit_3))
 print()
 print("Цифра в розряді одиниць:", int(input_digit_4))
+
+
+#### ДЗ 2.2
