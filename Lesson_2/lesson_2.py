3c8010f92eb106f4ae435b2226e207f756c1dc0a
Дмитро Лебедєв <lotustest@ua.fm>
2024-10-11 20:27:36 +0300

    Second lesson.

---
+++ Lesson_2/lesson_2.py
@@ -0,0 +1,17 @@
+# 274 => 2 7 4
+
+number = 667
+n1 = number // 100
+
+n2 = number % 100 // 10
+
+n3 = number % 10
+
+sum_n = n1 + n2 + n3
+
+print (number)
+print (n1)
+print (n2)
+print (n3)
+print (sum_n)
+
